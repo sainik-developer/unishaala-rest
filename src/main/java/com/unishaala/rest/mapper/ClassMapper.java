f0b9fb11d7978327a45b3717cf83ef07f81e5841
sainik <sainik.developer.2017@gmail.com>
2020-08-20 17:36:18 +0530

    admin class addition and updation is implemented

--- src/main/java/com/unishaala/rest/mapper/ClassMapper.java
+++ src/main/java/com/unishaala/rest/mapper/ClassMapper.java
@@ -2,14 +2,18 @@ package com.unishaala.rest.mapper;
 
 import com.unishaala.rest.dto.ClassDTO;
 import com.unishaala.rest.model.ClassDO;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = SchoolMapper.class)
 public interface ClassMapper {
     ClassMapper INSTANCE = Mappers.getMapper(ClassMapper.class);
 
     ClassDTO toDTO(final ClassDO classDO);
 
     ClassDO fromDTO(final ClassDTO classDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+            nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+    void update(@MappingTarget ClassDO toClassDO, ClassDTO fromClassDTO);
 }
